6b4d7642e875003b9c7b390165ec6ffefe6dbee4
NairBN <bnnair@yahoo.com>
2017-12-24 22:13:34 +0530

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into TODO values (1001, 'Learn spring cloud', false, sysdate(),'in28Minutes');
+insert into TODO values (1002, 'Learn spring Boot', false, sysdate(),'in28Minutes');
+insert into TODO values (1003, 'Learn to Ski', false, sysdate(),'in28Minutes');
+insert into TODO values (1004, 'Learn Machine Learning', false, sysdate(),'in28Minutes');
---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into TODO values (1001, 'Learn spring cloud', false, sysdate(),'in28Minutes');
+insert into TODO values (1002, 'Learn spring Boot', false, sysdate(),'in28Minutes');
+insert into TODO values (1003, 'Learn to Ski', false, sysdate(),'in28Minutes');
